41bb3fdeddffd1f731183d52d6054c423f49889b
Gyungwon Na <nkw601@naver.com>
2024-07-04 04:02:22 +0900

    숫자야구 게임 구현

--- 숫자야구 게임/script.js
+++ 숫자야구 게임/script.js
@@ -19,6 +19,33 @@ function clearInput() {
     });
 }
 
+function display_ending(success) {
+    //확인하기 버튼 비활성화
+    const game_btn = document.querySelector('.submit-button');
+    game_btn.classList.add('disabled');
+    game_btn.disabled = true;
+
+    //game-result div
+    const game_result_div = document.createElement('div');
+    game_result_div.className = 'game-result';
+
+    //이미지: id가 game-result-img인 img태그 사용
+    const game_result_img = document.createElement('img');
+    game_result_img.id = 'game-result-img';
+
+    if(success){
+        game_result_img.src = './success.png';
+    } else{
+        game_result_img.src = './fail.png';
+    }
+    game_result_div.appendChild(game_result_img);
+
+    //document/result-display 앞에 추가
+    const result_display = document.querySelector('.game-board');
+    result_display.insertBefore(game_result_div, null);
+}
+
+//공통 부분(left_div 생성) 묶기
 function display_left_div(user_inputs) {
     const left_div = document.createElement('div');
     left_div.className = 'left';
@@ -28,10 +55,6 @@ function display_left_div(user_inputs) {
 
 
 function display_result(user_inputs, strike_cnt, ball_cnt){
-    console.log(user_inputs);
-    console.log(strike_cnt, ball_cnt);
-    console.log(answers);
-    
     const check_result_div = document.createElement('div');
     check_result_div.className = 'check-result';
     left_div = display_left_div(user_inputs);
@@ -54,7 +77,6 @@ function display_result(user_inputs, strike_cnt, ball_cnt){
         </div>
         `;
     }
-
     check_result_div.appendChild(left_div);
     check_result_div.appendChild(document.createTextNode(':'));
     check_result_div.appendChild(right_div);
@@ -62,9 +84,10 @@ function display_result(user_inputs, strike_cnt, ball_cnt){
     const result_display = document.querySelector('.result-display');
     result_display.appendChild(check_result_div);
     
-    // if(strike_cnt == 3){
-    //     display_ending(true);
-    // }
+    //게임 종료 조건 1: 모두 스트라이크일 경우
+    if(strike_cnt == 3){
+        display_ending(true);
+    }
 }
 
 function check_answer(user_inputs) {
@@ -78,24 +101,17 @@ function check_answer(user_inputs) {
         if(user_inputs[i] == answers[i]){
             strike_cnt++;
         } else if(answers.includes(user_inputs[i])){
-            // console.log(user_inputs[i]);   
             ball_cnt++;
         }
     }
     display_result(user_inputs, strike_cnt, ball_cnt);
 }
 
-function display_ending(success) {
-    if(success){
-        console.log(true);
-    } else{
-        console.log(false);
-    }
-}
+
 //게임 초기화
 //시도 가능 횟수 설정
 const try_num = 9;
-let try_cnt = 0;
+let try_cnt = 1;
 
 //중복되지 않는 3개의 랜덤한 숫자 설정
 const answers = get_random_nums();
@@ -104,7 +120,6 @@ const answers = get_random_nums();
 document.addEventListener("DOMContentLoaded", function(){
     const start = document.querySelector('.result-display');
     start.innerHTML = '';
-    // console.log(answers);
     clearInput();
 });
 
@@ -113,7 +128,6 @@ function check_numbers(){
     num1 = document.getElementById('number1');
     num2 = document.getElementById('number2');
     num3 = document.getElementById('number3');
-    // console.log(num1.value, num2, num3);
     //입력되지 않은 input 확인
     if(num1.value == '' || num2.value == '' || num3.value == ''){
         clearInput();
@@ -123,13 +137,12 @@ function check_numbers(){
         //입력된 숫자들과 정답을 비교하여 결과를 생성하는 로직 만들기
         let user_inputs = [Number(num1.value), Number(num2.value), Number(num3.value)];
         //기회 남아있는지 확인
+        //게임 종료 조건 2, 더 이상 기회 없을 때 -> 버튼 9번 누를 수 있음
         if(try_cnt >= try_num){
-            // display_ending(false);
-            console.log('game over'); 
+            display_ending(false);
             }
         else {
             try_cnt++;
-            // console.log(user_inputs);
             check_answer(user_inputs);
             clearInput();
         }
