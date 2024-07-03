a0ba58beca4ff147e06db34af7adb23429755ed9
Gyungwon Na <nkw601@naver.com>
2024-07-04 00:16:27 +0900

    JavaScript-handling items

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -1,15 +1,31 @@
 //json -> item
 function loadItems() {
-    return fetch('data/data.json')
+    return fetch('./data/data.json')
     .then(response => response.json())
     .then(json => json.items);
+}
+
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    const html = items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+function createHTMLString(item) {
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail /">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+    `;
+    
 }
 
 //main
 loadItems()
 .then(items => {
     console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventLisners(items)
 })
 .catch(console.log);
