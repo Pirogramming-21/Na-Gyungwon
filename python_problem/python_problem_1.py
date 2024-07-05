c0b4939d7369ea8971dceacaa8a32e51e023d01c
Gyungwon Na <nkw601@naver.com>
2024-07-05 20:35:30 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,7 +3,7 @@
 num = 0
 
 # 2단계: input()으로 1~3 정수 입력
-# num_cnt = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+# num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
 
 # 3단계:
@@ -12,28 +12,30 @@ num = 0
 # 올바른 값이 입력될 때가지 입력 요구
 def input_num():
     while True:
-        num_cnt = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        num = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
 
-        if not num_cnt.isdigit():
+        if not num.isdigit():
             print("정수를 입력하세요.")
             continue
 
-        num_cnt = int(num_cnt)
+        num = int(num)
 
-        if num_cnt not in [1, 2, 3]:
+        if num not in [1, 2, 3]:
             print("1, 2, 3 중 하나를 입력하세요")
             continue
 
         break
-    return num_cnt
+    return num
 
 
-num_cnt = input_num()
+# num = input_num()
+
 # 4단계: num을 이용하여 2단계에서 입력한 수만큼 숫자를 출력하는 코드 작성
-cur_num = 1
-for i in range(num_cnt):
-    print(f"playerA: {cur_num}")
-    cur_num += 1
+
+# cur_num = 1
+# for i in range(num):
+#     print(f"playerA: {cur_num}")
+#     cur_num += 1
 
 # 5단계:
 # 1~3 사이의 정수를 입력받는 코드 작성 "부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능): "
@@ -41,29 +43,55 @@ for i in range(num_cnt):
 # 1,2,3을 입력하지 않는 경우 , "1, 2, 3 중 하나를 입력하세요" 출력
 # 올바른 값이 입력될 때가지 입력 요구
 # num을 이용하여 입력한 수만큼 숫자를 출력하는 코드 작성
-num_cnt = input_num()
 
-for i in range(num_cnt):
-    print(f"playerB: {cur_num}")
-    cur_num += 1
+# num = input_num()
+
+# for i in range(num):
+#     print(f"playerB: {cur_num}")
+#     cur_num += 1
 
 # 6단계: 게임이 끝날 때까지 playerA와 playerB에게 번갈아가며 부를 숫자의 개수를 입력받는 코드 작성
-players = ["playerA", "playerB"]
-cur_idx = 0
 
-while cur_num < 32:
-    num_cnt = input_num()
-    for i in range(num_cnt):
-        print(f"{players[cur_idx%2]}: {cur_num}")
-        cur_num += 1
-        if cur_num >= 32:
-            break
-    cur_idx += 1
+# players = ["playerA", "playerB"]
+# cur_idx = 0
+
+# while cur_num < 32:
+#     num = input_num()
+#     for i in range(num):
+#         print(f"{players[cur_idx%2]}: {cur_num}")
+#         cur_num += 1
+#         if cur_num >= 32:
+#             break
+#     cur_idx += 1
 
 # 7단계: 게임이 끝났을 때, 누가 이겼는지 출력
 # "playerA win!" / "playerB win!"
-print(f"{players[cur_idx%2]} win!")
+
+# print(f"{players[cur_idx%2]} win!")
+
+
 # 8단계: 6단계까지 중복되는 코드를 찾아 함수로 만들기, 함수 이름 "brGame"
+
+
+def brGame(players=["playerA", "playerB"]):
+    cur_idx = 0
+    cur_num = 1
+
+    while cur_num < 32:
+        num = input_num()
+        for i in range(num):
+            print(f"{players[cur_idx%2]}: {cur_num}")
+            cur_num += 1
+            if cur_num >= 32:
+                break
+        cur_idx += 1
+
+    print(f"{players[cur_idx%2]} win!")
+    # 승리한 player 이름 Return
+    # return players[cur_idx%2]
+
+
+brGame()
 
 # 9단계: computer과 대결하는 배스킨라빈스31게임 제작
 # playerA -> computer, playerB -> player
