51f269cf4baec3faeef5a0e1a06d063183308bdf
Gyungwon Na <nkw601@naver.com>
2024-07-05 23:08:47 +0900

    css 수정

--- Javascript 스톱워치 과제/script.js
+++ Javascript 스톱워치 과제/script.js
@@ -1,1 +1,7 @@
 // script
+const StartBtn = getElementById('btn__start');
+const StopBtn = getElementById('btn__stop')
+const ResetBtn = getElementById('btn__reset')
+
+let sec = 0;
+let censec = 0;
