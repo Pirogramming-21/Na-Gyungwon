ca160a074402287438d126cf8d1f537ac02dca9f
Gyungwon Na <nkw601@naver.com>
2024-07-13 03:53:24 +0900

    챌린지 살짝 도전

--- myMovieReviews/myMovieReviews/forms.py
+++ myMovieReviews/myMovieReviews/forms.py
@@ -6,6 +6,7 @@ class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
         fields = (
+            "poster",
             "title",
             "release_year",
             "genre",
@@ -14,5 +15,36 @@ class MovieForm(forms.ModelForm):
             "review",
             "director",
             "actor",
-            "poster",
         )
+        widgets = {
+            "poster": forms.ClearableFileInput(attrs={"class": "review__form"}),
+            "title": forms.TextInput(
+                attrs={"class": "review__form", "placeholder": "제목"}
+            ),
+            "release_year": forms.NumberInput(
+                attrs={"class": "review__form", "placeholder": "개봉년도"}
+            ),
+            "genre": forms.Select(attrs={"class": "review__form"}),
+            "rating": forms.NumberInput(
+                attrs={"class": "review__form", "placeholder": "별점"}
+            ),
+            "running_time": forms.NumberInput(
+                attrs={"class": "review__form", "placeholder": "러닝타임"}
+            ),
+            "review": forms.Textarea(
+                attrs={"class": "review__form", "placeholder": "리뷰"}
+            ),
+            "director": forms.TextInput(
+                attrs={"class": "review__form", "placeholder": "감독"}
+            ),
+            "actor": forms.TextInput(
+                attrs={"class": "review__form", "placeholder": "배우"}
+            ),
+        }
+
+
+# https://docs.djangoproject.com/en/5.0/ref/forms/widgets/
+# class CommentForm(forms.Form):
+#     name = forms.CharField(widget=forms.TextInput(attrs={"class": "special"}))
+#     url = forms.URLField()
+#     comment = forms.CharField(widget=forms.TextInput(attrs={"size": "40"}))
--- myMovieReviews/myMovieReviews/models.py
+++ myMovieReviews/myMovieReviews/models.py
@@ -4,10 +4,26 @@ from django import forms
 
 # Create your models here.
 class Movie(models.Model):
+    genre_choices = [
+        # ('DB저장', 'display')
+        ("action", "액션"),
+        ("animation", "애니메이션"),
+        ("comedy", "코미디"),
+        ("documentary", "다큐멘터리"),
+        ("drama", "드라마"),
+        ("fantasy", "판타지"),
+        ("horror", "호러"),
+        ("musical", "뮤지컬"),
+        ("romance", "로맨스"),
+        ("sf", "SF"),
+        ("thriller", "스릴러"),
+        ("historical", "사극"),
+    ]
+    # https://ugaemi.com/django/Django-choices-field/
     poster = models.ImageField(upload_to="", blank=True, default="default.jpg")
     title = models.CharField(max_length=300, default="")
     release_year = models.IntegerField(default=0)
-    genre = models.CharField(max_length=300, default="")
+    genre = models.CharField(max_length=100, choices=genre_choices, default="")
     rating = models.FloatField(default=0)
     running_time = models.IntegerField(default=0)
     review = models.CharField(max_length=1000, default="")
